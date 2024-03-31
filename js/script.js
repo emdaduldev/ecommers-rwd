902a880da65512ea8e6c550942cb8d3147415d88
emdaduldev <emdadulrahman776@gmail.com>
2024-03-31 15:13:30 +0600

    ecommerce done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.partners-wrapper').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    autoplay: false,
+    arrows: true,
+    prevArrow: '.one',
+    nextArrow: '.two',
+    centerMode: true,
+    centerPadding: '0px',
+  });
